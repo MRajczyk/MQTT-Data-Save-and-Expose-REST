b77203a64049870b06652f51ca92a525fc7198ee
MRajczyk <mikolajrajczyk01@gmail.com>
2023-05-08 14:04:02 +0200

    added returning correct json for all data

--- MQTT.py
+++ MQTT.py
@@ -5,8 +5,6 @@ from MQTTDbConn import MQTTDbConn
 from fastapi import FastAPI
 import uvicorn
 
-import json
-
 
 class MQTTData(BaseModel):
     log_id: int
@@ -18,6 +16,8 @@ class MQTTData(BaseModel):
 
 
 if __name__ == "__main__":
+    types = ['temp', 'lum']
+
     mqttClient = MQTTDbConn("testdb", "127.0.0.1", 1883, "dev/#")
     mqttClient.start()
 
@@ -43,16 +43,24 @@ if __name__ == "__main__":
 
         # extract min and max data (begin, end) - ok
         # TODO: build json according to szymonaszek's mock (to be talked about)
+        ret_data = []
 
-        data = {}
-        # data['datetime'] = {'from': macs[0], 'to': macs[1], 'timezone_offset': 7200}
-        json_data = json.dumps(data)
-        # ret = mqttClient.getAllData()
-        # data = []
-        # for row in ret:
-        #     data.append(MQTTData(log_id=row[0], timestamp=row[1], mac_addr=row[2], type=row[3], sensor_id=row[4], reading=row[5]))
+        for mac in macs:
+            tmp_dict = {'datetime': {'from': mac[1], 'to': mac[2], 'timezone_offset': 7200}}
+            tmp_dict['sensor'] = {"mac_address": mac[0], "localization": 'tbd'}
+            data = []
+            for type in types:
+                dataForType = {'name': type}
+                measurement_list = []
+                for mac_row in mqttClient.getAllDataForSensor(mac[0]):
+                    if mac_row[3] == type:
+                        measurement_list.append({'datetime': mac_row[1], 'result': mac_row[5]})
+                dataForType['measurements'] = measurement_list
+                data.append(dataForType)
+            tmp_dict['data'] = data
+            ret_data.append(tmp_dict)
 
-        return json_data
+        return ret_data
 
 
     @app.get("/data/test", response_model=List[MQTTData])
