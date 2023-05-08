468f575f23d402a0e222b56b6a4f31e026c3aaa2
MRajczyk <mikolajrajczyk01@gmail.com>
2023-05-08 10:07:55 +0200

    added selecting data by mac addr method, listing all mac adresses

--- MQTT.py
+++ MQTT.py
@@ -7,13 +7,16 @@ import uvicorn
 
 
 class MQTTData(BaseModel):
-    id: str
-    topic: str
-    data: str
+    log_id: int
+    timestamp: int
+    mac_addr: str
+    type: str
+    sensor_id: int
+    reading: float
 
 
 if __name__ == "__main__":
-    mqttClient = MQTTDbConn("testdb", "127.0.0.1", 1883, ("test", "test2"))
+    mqttClient = MQTTDbConn("testdb", "127.0.0.1", 1883, "dev/#")
     mqttClient.start()
 
     app = FastAPI()
@@ -22,12 +25,44 @@ if __name__ == "__main__":
     async def root():
         return {"message": "Hello World."}
 
-    @app.get("/data", response_model=List[MQTTData])
+
+    @app.get("/data/all", response_model=List[MQTTData])
     async def root():
-        ret = mqttClient.returnAllData()
+        macs = []
+        for row in mqttClient.getUniqueMacAddresses():
+            macs.append(row[0])
+
+        print(macs)
+
+        for mac in macs:
+            for mac_row in mqttClient.getAllDataForSensor(mac):
+                print(mac_row)
+            print("===")
+
+        # ret = mqttClient.getAllData()
+        # data = []
+        # for row in ret:
+        #     data.append(MQTTData(log_id=row[0], timestamp=row[1], mac_addr=row[2], type=row[3], sensor_id=row[4], reading=row[5]))
+
+        return [] # data
+
+
+    @app.get("/data/test", response_model=List[MQTTData])
+    async def root():
+        ret = mqttClient.getAllDataTest()
         data = []
         for row in ret:
-            data.append(MQTTData(id=row[0], topic=row[1], data=row[2]))
+            data.append(MQTTData(log_id=row[0], timestamp=row[1], mac_addr=row[2], type=row[3], sensor_id=row[4], reading=row[5]))
+
+        return data
+
+    @app.get("/data/temp", response_model=List[MQTTData])
+    async def root():
+        ret = mqttClient.getTempData()
+        data = []
+        for row in ret:
+            data.append(MQTTData(log_id=row[0], timestamp=row[1], mac_addr=row[2], type=row[3], sensor_id=row[4],
+                                 reading=row[5]))
 
         return data
 
--- MQTTDbConn.py
+++ MQTTDbConn.py
@@ -2,22 +2,31 @@ import paho.mqtt.client as paho
 import DbConnection as Db
 import threading
 import util
+import time
 
+
+# dev/{mac}/{type}/{id} - wiadomosc od urządzeń
+# msg i w body polecenie
+
+# suboskrybowanie wildcarda dev/#
+
+# # - multi level wildcard
+# + - single level wildcard
+
+# no i zapis do bazki mac - adres mac urządzenia, type - jakis predefiniowany typ, id
+# create table mqtt_logs (log_id INTEGER PRIMARY KEY AUTOINCREMENT, timestamp INTEGER NOT NULL, mac_addr TEXT NOT NULL, type TEXT, sensor_id INTEGER, reading REAL NOT NULL)
 
 class MQTTDbConn(threading.Thread):
-    # def __init__(self, dbName):
     def __init__(self, dbname, ipAddr, port, topics):
         threading.Thread.__init__(self)
-
         self.dbName = dbname
         self.ipAddr = ipAddr
         self.port = port
         self.topics = topics
+        self.dbConn = Db.DbConnection(self.dbName)
+        self.client = paho.Client()
 
     def run(self):
-        self.dbConn = Db.DbConnection(self.dbName)
-
-        self.client = paho.Client()
         self.client.on_message = self.on_message
         self.client.on_publish = self.on_publish
         self.client.connect(self.ipAddr, self.port, 60)
@@ -32,22 +41,36 @@ class MQTTDbConn(threading.Thread):
         # print("===== ALL DATA STORED IN THE DATABASE: =====")
         # self.printLogs()
         # print('')
-        self.saveMessage(msg.topic, util.convertBytesToString(msg.payload))
+
+        topicTokens = msg.topic.split('/')
+        self.saveMqttLog(topicTokens[1], topicTokens[2], topicTokens[3], float(util.convertBytesToString(msg.payload)))
 
     def on_publish(self, mosq, obj, mid):
         pass
 
-    def returnAllData(self):
+    def getUniqueMacAddresses(self):
+        return self.dbConn.executeSQL("SELECT DISTINCT mac_addr FROM mqtt_logs")
+
+    def getAllDataForSensor(self, mac_addr):
+        return self.dbConn.executeSQL(f"SELECT * FROM mqtt_logs WHERE mac_addr='{mac_addr}'")
+
+    def getAllDataTest(self):
         return self.dbConn.executeSQL("SELECT * FROM mqtt_logs")
 
-    def deleteAllData(self):
-        return self.dbConn.executeSQL("DELETE FROM mqtt_logs WHERE id != -1")
+    def getAllData(self):
+        return self.dbConn.executeSQL("SELECT * FROM mqtt_logs")
 
-    def printLogs(self):
+    def getTempData(self):
+        return self.dbConn.executeSQL("SELECT * FROM mqtt_logs WHERE type = 'temp'")
+
+    def deleteAllData(self):
+        return self.dbConn.executeSQL("DELETE FROM mqtt_logs WHERE log_id != -1")
+
+    def printAllLogs(self):
         for row in self.dbConn.executeSQL("SELECT * FROM mqtt_logs"):
             print(row)
 
-    def saveMessage(self, mess_topic, mess_payload):
-        params = (mess_topic, mess_payload)
-        self.dbConn.executeSQL(f"INSERT INTO mqtt_logs VALUES (NULL, ?, ?)", params)
+    def saveMqttLog(self, mac, type, id, reading):
+        params = (int(time.time()), mac, type, id, reading)
+        self.dbConn.executeSQL(f"INSERT INTO mqtt_logs VALUES (null, ?, ?, ?, ?, ?)", params)
         self.dbConn.commit()
